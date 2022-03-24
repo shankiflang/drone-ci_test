a01016fe9dfa4888116d1db99800166a4d35734c
Benjamin BENOIT <shankiflang@gmail.com>
2022-03-24 11:30:17 +0100

    Testing env

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          { process.env.NODE_ENV || "YA PAS OUESSSH" }
+          { process.env.NODE_TEXT || "YA PAS OUESSSH" }
         </p>
         <a
           className="App-link"
