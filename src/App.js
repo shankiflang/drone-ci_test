692043a9320ab5a7ebe3fabe66f088f6b342869b
Benjamin BENOIT <shankiflang@gmail.com>
2022-05-14 12:06:05 +0200

    Adding Nicolas to code

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          { process.env.NODE_TEXT || "YA TOUJOURS PAS DE CACAHUETES, MAIS YA LA PROD" }
+          { process.env.NODE_TEXT || "Coucou Nicolas" }
         </p>
         <a
           className="App-link"
