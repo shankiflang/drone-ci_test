d033e21534ccd32b42783b3d383fad9242a6085d
Benjamin BENOIT <shankiflang@gmail.com>
2022-05-14 11:50:26 +0200

    Adding pushing trigger

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          { process.env.NODE_TEXT || "YA TOUJOURS PAS DE CACAHUETES" }
+          { process.env.NODE_TEXT || "YA TOUJOURS PAS DE CACAHUETES, MAIS YA LA PROD" }
         </p>
         <a
           className="App-link"
